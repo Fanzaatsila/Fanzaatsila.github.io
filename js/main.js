064f24222e2f96be1d5efc48ef565ef3d6229237
Fanza Atsila Fizarli <fanzaatsila@gmail.com>
2024-02-27 22:57:34 +0700

    Finalisasi Web Profile

--- js/main.js
+++ js/main.js
@@ -1,6 +1,32 @@
- const mntoggle = document.querySelector('.menu-toggle input');
- const nav = document.querySelector('nav ul');
+const menuToggle = document.querySelector('.menu-toggle');
+const slider = document.querySelector('.slider');
 
-mntoggle.addEventListener('click',function(){
-    nav.classList.toggle('menushow');
-})
+menuToggle.addEventListener('click', function() {
+    const dropdownContent = document.querySelector('.dropdown-content');
+    dropdownContent.classList.toggle('show');
+
+    // Sembunyikan slider saat menu toggle diklik
+    if (slider.style.display === 'none') {
+        slider.style.display = 'block';
+    } else {
+        slider.style.display = 'none';
+    }
+});
+
+function updateProgressBar(elementId, percentage) {
+    const circle = document.getElementById(elementId).querySelector('.progress-bar');
+    const label = document.getElementById(elementId).querySelector('.progress-label');
+    const radius = circle.r.baseVal.value;
+    const circumference = 2 * Math.PI * radius;
+    const offset = circumference - (percentage / 100) * circumference;
+    circle.style.strokeDasharray = `${circumference}`;
+    circle.style.strokeDashoffset = offset;
+    label.textContent = `${percentage}%`;
+}
+
+// Call the function for each progress bar
+updateProgressBar('progress-bar-1', 90); // Laravel - 90%
+updateProgressBar('progress-bar-2', 80); // Laravel - 90%
+
+
+
